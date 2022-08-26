4ee1e6ae94c35dc047175f67f628ea5d0a197237
Jude Antony <judebantony@gmail.com>
2022-08-26 17:43:31 +0530

    JUDE-85 fixed the error

--- src/main/java/com/jba/release/endpoints/EmployeeRestController.java
+++ src/main/java/com/jba/release/endpoints/EmployeeRestController.java
@@ -1,10 +1,24 @@
 package com.jba.release.endpoints;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.jba.release.model.Employee;
+import com.jba.release.services.EmployeeService;
 
 @RestController(value = "/api/v1/employee")
 public class EmployeeRestController {
     
+    @Autowired
+    private EmployeeService employeeService;
 
+    @GetMapping("/name/{id}")
+    public Employee getEmployeeName(String name) {
+        return employeeService.getEmployee(name);
+    }
+    
+
+    
 
 }
